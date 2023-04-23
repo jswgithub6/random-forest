1d6005b0abd1d414ce7220c39ac3077c5e147afd
wjs <1530466272@qq.com>
2023-04-23 18:13:11 +0800

    feat: update

--- data.js
+++ data.js
@@ -86,6 +86,63 @@ function getData() {
   })
 }
 
+function processData(data) {
+  const {
+    PDJ_TN,
+    PDJ_TP, 
+    PDJ_NH,
+    PDJ_SS, 
+    PDJ_PH, 
+    PDJ_COD,
+    GXC_TN,
+    GXC_TP,
+    GXC_NH, 
+    GXC_SS, 
+    GXC_COD,
+    JYJ_C2,
+    JYJ_C3,
+    JYJ_C4
+  } = data
+  const dataset = []
+  // 数据集生成
+  const min = Math.min(PDJ_TN.length,
+  PDJ_TP.length,
+  PDJ_NH.length,
+  PDJ_SS.length ,
+  PDJ_PH.length ,
+  PDJ_COD.length,
+  GXC_TN.length,
+  GXC_TP.length,
+  GXC_NH.length,
+  GXC_SS.length,
+  GXC_COD.length,
+  JYJ_C2.length,
+  JYJ_C3.length,
+  JYJ_C4.length)
+  let i = 0
+  while(i < min) {
+    dataset.push([
+      PDJ_TN[i].VAL,
+      PDJ_TP[i].VAL, 
+      PDJ_NH[i].VAL,
+      PDJ_SS[i].VAL, 
+      PDJ_PH[i].VAL, 
+      PDJ_COD[i].VAL,
+      GXC_TN[i].VAL,
+      GXC_TP[i].VAL,
+      GXC_NH[i].VAL, 
+      GXC_SS[i].VAL, 
+      GXC_COD[i].VAL,
+      JYJ_C2[i].VAL,
+      JYJ_C3[i].VAL,
+      JYJ_C4[i].VAL
+    ].map(Number))
+    i++
+  }
+  return dataset
+}
+
 module.exports = {
-  getData
+  getData,
+  processData
 }
--- train-5.js
+++ train-5.js
@@ -1,63 +1,6 @@
 const { writeFileSync } = require('fs');
-const { getData } = require('./data');
+const { getData, processData } = require('./data');
 const RFRegression = require('ml-random-forest').RandomForestRegression
-
-function processData(data) {
-  const {
-    PDJ_TN,
-    PDJ_TP, 
-    PDJ_NH,
-    PDJ_SS, 
-    PDJ_PH, 
-    PDJ_COD,
-    GXC_TN,
-    GXC_TP,
-    GXC_NH, 
-    GXC_SS, 
-    GXC_COD,
-    JYJ_C2,
-    JYJ_C3,
-    JYJ_C4
-  } = data
-  const dataset = []
-  // 数据集生成
-  const min = Math.min(PDJ_TN.length,
-  PDJ_TP.length,
-  PDJ_NH.length,
-  PDJ_SS.length ,
-  PDJ_PH.length ,
-  PDJ_COD.length,
-  GXC_TN.length,
-  GXC_TP.length,
-  GXC_NH.length,
-  GXC_SS.length,
-  GXC_COD.length,
-  JYJ_C2.length,
-  JYJ_C3.length,
-  JYJ_C4.length)
-  let i = 0
-  while(i < min) {
-    dataset.push([
-      PDJ_TN[i].VAL,
-      PDJ_TP[i].VAL, 
-      PDJ_NH[i].VAL,
-      PDJ_SS[i].VAL, 
-      PDJ_PH[i].VAL, 
-      PDJ_COD[i].VAL,
-      GXC_TN[i].VAL,
-      GXC_TP[i].VAL,
-      GXC_NH[i].VAL, 
-      GXC_SS[i].VAL, 
-      GXC_COD[i].VAL,
-      JYJ_C2[i].VAL,
-      JYJ_C3[i].VAL,
-      JYJ_C4[i].VAL
-    ].map(Number))
-    i++
-  }
-  return dataset
-}
-
 
 ;(async function () {
   const data = await getData()
@@ -68,12 +11,12 @@ function processData(data) {
   const predictions = new Array(dataset.length);
   
   for (let i = 0; i < dataset.length; ++i) {
-    trainingSet[i] = dataset[i];
+    trainingSet[i] = dataset[i].slice(0, 11);
     predictions[i] = dataset[i][11];
   }
   const options = {
     seed: 3,
-    maxFeatures: 14,
+    maxFeatures: 11,
     replacement: false,
     nEstimators: 200
   };
